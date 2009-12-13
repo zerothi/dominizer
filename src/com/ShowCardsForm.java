c5977f57aa900749fd63839ac5dbacef58ecd7ac
Nick Papior Andersen <nickpapior@gmail.com>
2009-12-13 20:50:30 +0000

    Type and coins added. Two different approaches now available

--- src/com/ShowCardsForm.java
+++ src/com/ShowCardsForm.java
@@ -47,7 +47,7 @@ public class ShowCardsForm extends Form  implements CommandListener {
 
 	public void viewCards(Cards cards) {
 		this.removeCommand(blackMarketCmd);
-		this.table.setDimension(3, cards.size() + 1);
+		this.table.setDimension(4, cards.size() + 1);
 		//#debug
 		System.out.println("adding header");
 		//#style tableHeading
@@ -56,9 +56,14 @@ public class ShowCardsForm extends Form  implements CommandListener {
 		this.table.set(1, 0, Locale.get("table.heading.Expansion"));
 		//#style tableHeading
 		this.table.set(2, 0, Locale.get("table.heading.Cost"));
+		//#style tableHeading
+		this.table.set(3, 0, Locale.get("table.heading.Type"));
 		//#debug
 		System.out.println("adding card information");
 		for (int cardNumber = 0 ; cardNumber < cards.size() ; cardNumber++ ) {
+			//#style tableCell
+			this.table.set(0, cardNumber + 1, cards.getName(cardNumber));
+			/*
 			switch ( cards.getCardType(cardNumber) ) {
 			case Cards.TYPE_ACTION:
 				//#style cardTypeAction
@@ -93,6 +98,7 @@ public class ShowCardsForm extends Form  implements CommandListener {
 				this.table.set(0, cardNumber + 1, cards.getName(cardNumber) );
 				break;
 			}
+			*/
 			if ( cards.getName(cardNumber).equals(Locale.get("card.BlackMarket")) )
 				this.addCommand(blackMarketCmd);
 			try {
@@ -109,6 +115,40 @@ public class ShowCardsForm extends Form  implements CommandListener {
 			} catch (IOException e) {
 				//#style tableCellCentered
 				this.table.set(2, cardNumber + 1, new Integer(cards.getCost(cardNumber)));
+			}
+			switch ( cards.getCardType(cardNumber) ) {
+			case Cards.TYPE_ACTION:
+				//#style cardTypeAction
+				this.table.set(3, cardNumber + 1, " A ");
+				break;
+			case Cards.TYPE_ACTION_ATTACK:
+				//#style cardTypeActionAttack
+				this.table.set(3, cardNumber + 1, "A+A" );
+				break;
+			case Cards.TYPE_ACTION_REACTION:
+				//#style cardTypeActionReaction
+				this.table.set(3, cardNumber + 1, "A+R" );
+				break;
+			case Cards.TYPE_VICTORY:
+				//#style cardTypeVictory
+				this.table.set(3, cardNumber + 1, " V " );
+				break;
+			case Cards.TYPE_ACTION_TREASURY:
+				//#style cardTypeActionTreasury
+				this.table.set(3, cardNumber + 1, "A+T" );
+				break;
+			case Cards.TYPE_ACTION_VICTORY:
+				//#style cardTypeActionVictory
+				this.table.set(3, cardNumber + 1, "A+V" );
+				break;
+			case Cards.TYPE_TREASURY_VICTORY:
+				//#style cardTypeTreasuryVictory
+				this.table.set(3, cardNumber + 1, "T+V" );
+				break;
+			case Cards.TYPE_ACTION_DURATION:
+				//#style cardTypeActionDuration
+				this.table.set(3, cardNumber + 1, "A+D" );
+				break;
 			}
 		}
 	}
